92c907e56d5592945127c2f4054151693b09ce81
staka99 <marko.staka12@gmail.com>
2024-09-28 02:01:11 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <header> 
+    <nav class="d-flex justify-content-center 
+    bglight border border-bottom 
+    border-dark p-3 w-100">
+    <RouterLink to="/ChatRoom1">ChatRoom1</RouterLink> | 
+    <RouterLink to="/ChatRoom2">ChatRoom2</RouterLink>
+    </nav> 
+  </header> 
+  <RouterView/>
+ <div id="app">
+  <MessageComponent :messages="messages" />
+  <ChatInput @message-sent="addMessage" />
+  <UserProfile />
+  </div>
+</template>
+
+<script>
+import MessageComponent from "@/components/MessageComponent.vue";
+import ChatInput from "@/components/ChatInput.vue";
+import UserProfile from '@/components/UserProfile';
+
+
+
+export default {
+  components: {
+    MessageComponent,
+    ChatInput,
+    UserProfile,
+  },
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  methods: {
+    addMessage({ username, message }) {
+      // Format the message with the username
+      const formattedMessage = `${username}: ${message}`;
+      this.messages.push(formattedMessage);
+  },
+},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+
+const path = require('path');
+
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
+  },
+  transpileDependencies: true
+}; 
+
+module.exports = {
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      'example.com',
+      '192.168.1.1',
+    ],
+  },
+};
